177ac1075ffdfe0b13a5c15d2271d7d041b7b39c
dasberg <dasberg@kth.se>
2022-12-06 11:50:50 +0100

    Did day06

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashSet;
+use std::fs;
+
+fn read_input(file: &str) -> Vec<char> {
+    let input = fs::read_to_string(file).expect("Something went wrong reading the file");
+    input.chars().collect()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn find_first_marker(message: Vec<char>, window_size: usize) {
+    for (i, window) in message.windows(window_size).enumerate() {
+        // if all the characters are unique, print the index and break
+        if window.iter().collect::<HashSet<_>>().len() == window_size {
+            println!("{}", i + window_size);
+            break;
+        }
     }
+}
+
+pub fn part1() {
+    let message = read_input("input/day06.in");
+    find_first_marker(message, 4);
+}
+
+pub fn part2() {
+    let message = read_input("input/day06.in");
+    find_first_marker(message, 14);
 }
